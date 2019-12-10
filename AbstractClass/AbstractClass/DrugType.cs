a8fa7733f332d3c55ddeb418a41f0192fa10df33
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-12-10 15:05:30 +0400

    Add files via upload

---
+++ AbstractClass/AbstractClass/DrugType.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class DrugType:object
+    {
+        private static int _id;
+        public string Name;
+        public int Id;
+
+
+        public DrugType(string name)
+        {
+            _id++;
+            Id = _id;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DrugType type1 = new DrugType("headache");
+            DrugType type2 = new DrugType("stomachache");
+            Console.WriteLine(type1.Id);
+            Console.WriteLine(type2.Id);
+        }
+    }
+}
